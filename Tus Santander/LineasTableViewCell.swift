c9b17f57ed3d30de1a65c6920d93c7113bf3b2ae
Luis Martin <luis_martindelarubia@hotmail.com>
2017-11-14 17:36:17 +0100

    dafafa

---
+++ Tus Santander/LineasTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  LineasTableViewCell.swift
+//  Tus Santander
+//
+//  Created by Luis Martin on 14/11/17.
+//  Copyright © 2017 Luis Martin. All rights reserved.
+//
+
+import UIKit
+
+class LineasTableViewCell: UITableViewCell {
+    //MARK: Properties
+
+    @IBOutlet weak var numeroLabel: UILabel!
+    @IBOutlet weak var nombreLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
