d2490b32749e77243f3947b83a83c0dc1631f511
Luis Martin <luis_martindelarubia@hotmail.com>
2017-11-14 17:35:55 +0100

    comenzada implementación

---
+++ Tus Santander/Linea.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  Tus Santander
+//
+//  Created by Luis Martin on 14/11/17.
+//  Copyright © 2017 Luis Martin. All rights reserved.
+//
+
+import Foundation
+
+class Linea{
+    var numero: String
+    var nombre: String
+    var identificador: String
+    
+    
+    init(num:String, nom:String, id:String) {
+        self.numero=num
+        self.nombre=nom
+        self.identificador=id
+    }
+}
